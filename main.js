aba09e4b334baf0838746f9e5aec47b1147c319c
Juan <juanfgort@gmail.com>
2021-05-17 10:39:58 -0500

    submit funcionando 100%

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 const path = require('path');
 const router = express.Router();
 var db = require('./public/db');
 
 
 app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-    res.send('Página de inicio de nuestro Wiki');
+    res.sendFile(path.join(__dirname+'/public/characters.html'));
 });
 
 
@@ -21,28 +23,32 @@ app.get('/createcharacter', function(req, res) {
     res.sendFile(path.join(__dirname+'/public/character_create.html'));
 });
 
-app.post('/dbsignup', function(req, res) {
-    var ok = db.dbConnect(req.body);
-    if(ok===1){
-        res.sendFile(path.join(__dirname+'/public/main.html'));
-    }
+app.get('/characters', function(req, res) {
+    res.sendFile(path.join(__dirname+'/public/characters.html'));
 });
+
 
 app.post('/characterInsert', function(req, res) {
-    console.log(req);
     db.characterInsert(req);
-    res.sendFile(path.join(__dirname+'/public/main.html'));
+    res.sendFile(path.join(__dirname+'/public/characters.html'));
 });
 
-app.post('/dblogin', async function(req, res) {
-    var ok = await db.dbGetConnect(req.body);
-    if(ok!=0){
-        res.sendFile(path.join(__dirname+'/public/main.html'));
-    }
-    else{
-        res.redirect('/');
-    }
+app.get('/getcharacters', function(req, res) {
+    var ppl = db.getCharacters();
+    return ppl;
 });
+
+app.post('/characterDelete', function(req, res) {
+    db.delCharacter(req);
+    res.sendFile(path.join(__dirname+'/public/characters.html'));
+});
+
+app.post('/characterEdit', function(req, res) {
+    db.modCharacter(req);
+    res.sendFile(path.join(__dirname+'/public/characters.html'));
+});
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
--- public/db.js
+++ public/db.js
@@ -1,24 +1,13 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = "";
+const uri = "mongodb+srv://dndadmin:.DnDadm1n.@proyectofinal.heiln.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 module.exports = {
-    dbConnect:  async function dbConnect(body) {
-        try {
-            const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true,});
-          const collection = client.db("Users").collection("users");
-          await collection.insertOne({ user: body.uname, password: body.pwd});          
-          client.close();
-          return 1;
-        } catch (error) {
-            console.log(error);
-        }
-    },
 
-    characterInsert:  async function characterInsert(body) {
+    characterInsert:  async function characterInsert(request) {
         try {
             const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true,});
           const collection = client.db("Users").collection("characters");
-          console.log(body);
+          var body = request.body;
           await collection.insertOne({ name: body.name, class: body.class,level:body.level,race:body.race,ability_scores:body.abilityScores,s_throws:body.saveThrows, skills:body.skills,proficiencies:body.proficiencies,hp:body.hp,languages:body.languages,traits:body.traits,speed:body.speed,ab:body.ability_bonus,pb:body.prof_bonus,feats:body.feats,spells:body.spells,spell_slots:body.spellSlots});          
           client.close();
         } catch (error) {
@@ -26,21 +15,51 @@ module.exports = {
         }
     },
 
-    dbGetConnect:  async function dbGetConnect(body) {
+    getCharacters: async function getCharacters()
+    {
         try {
-          const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true,});
-          const collection = client.db("Users").collection("users");
-          var items = await collection.find({ user: body.uname, password: body.pwd}).toArray();
-
-          if(items.length==0 || typeof items == undefined)
-          {
-              throw new Error("User not found");
-          }          
-          client.close();
-        } catch (error) {
+            const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true,});
+            const collection = client.db("Users").collection("characters");
+            var items = await collection.find({}).toArray();
+            console.log(items);
+            client.close();
+            return items;
+        }
+        catch(error) {
             console.error(error);
             return 0;
         }
+
+    },
+
+    delCharacter: async function delCharacter(request)
+    {
+        try {
+            const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true,});
+            const collection = client.db("Users").collection("characters");
+            await collection.deleteOne({name:request.body.name});
+            client.close();
+        }
+        catch(error) {
+            console.error(error);
+            return 0;
+        }
+
+    },
+
+    modCharacter: async function modCharacter(request)
+    {
+        try {
+            const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true,});
+            const collection = client.db("Users").collection("characters");
+            await collection.updateOne({name:request.body.oldName},{ $set: {name: request.body.newName}});
+            client.close();
+        }
+        catch(error) {
+            console.error(error);
+            return 0;
+        }
+
     }
 };   
 
