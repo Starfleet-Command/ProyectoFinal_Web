093f78ffa04d8ed2ced65551a2f0405456c6854a
Sergio Hernandez <shc010013@gmail.com>
2021-05-23 23:14:59 -0500

    Added p/c functionality to pass around userClass

--- react-app/src/components/CharacterCreate.jsx
+++ react-app/src/components/CharacterCreate.jsx
@@ -27,11 +27,13 @@ class ClassSelection extends Component {
 
     handleChange(event) {
         this.setState({
-            input: event.target.value
+            input: event.target.value // Also call getclassinfo
         })
+
+        this.props.parentCallback(this.input);
     }
 
-    getClassInfo(input) {
+    async getClassInfo(input) {
         return fetch("https://www.dnd5eapi.co/api/classes/"+input)
             .then(response => response.json())
             .then(responseJson => {
@@ -122,17 +124,234 @@ class ClassSelection extends Component {
                 </Select>
                 <FormHelperText>Select your class</FormHelperText>
             </FormControl>
-        )
+        );
     }
-}
+};
+
+class LevelSelection extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            level: '',
+            userClass: this.props.parentToChild
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            level: event.target.value
+        })
+        
+        this.getLevel(this.userClass, this.level);
+        
+    }
+
+    async getSpellsByLevel(userClass, level) {
+        try {
+            const response = await fetch("https://www.dnd5eapi.co/api/classes/" + userClass + "/spells/");
+            const responseJson = await response.json();
+            var classpells = responseJson.results;
+            const lvlresponse = await fetch("https://www.dnd5eapi.co/api/spells?level=" + level);
+            const lvlresponse_1 = await lvlresponse.json();
+            var lvlspells = lvlresponse_1.results;
+            var innerjoin = [];
+            for (let i = 0; i < classpells.length; i++) {
+
+                for (let j = 0; j < lvlspells.length; j++) {
+                    const element = lvlspells[j].name;
+
+                    if (classpells[i].name === element) {
+                        innerjoin.push(classpells[i]);
+                    }
+
+                }
+
+
+            }
+            return innerjoin;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async getLevel(userClass, level) {
+        if(userClass!=="Choose a class") {
+            character.level = level;
+        return fetch("https://www.dnd5eapi.co/api/classes/"+userClass+"/levels/"+level)
+            .then(response => response.json())
+            .then(responseJson => {
+                var spells = responseJson.spellcasting;
+                var features = responseJson.features;
+                var class_specifics = responseJson.class_specific;  
+                var ability_bonus = responseJson.ability_score_bonuses;
+                var prof_bonus = responseJson.prof_bonus;
+
+                character.ability_bonus = ability_bonus;
+                character.prof_bonus = prof_bonus;
+                character.feats = [];
+                character.spellSlots = [];
+
+                var ab = document.getElementById("abilityBonus")
+                ab.innerHTML= "<br>Your ability scores (INT, DEX, etc) get a bonus of +"+ability_bonus;
+
+                var pb = document.getElementById("scoreBonus")
+                pb.innerHTML= "<br>Skills you are proficient in get a bonus of +"+prof_bonus;
+
+                var fts = document.getElementById("feats");
+                fts.innerHTML="";
+                fts.innerHTML+= "<br> You have the following features (consult a 5e manual for more info) <br>";
+                if(features.length>0)
+                {
+                    for (let index = 0; index < features.length; index++) {
+                    fts.innerHTML+=features[index].name+"<br>";
+                    character.feats.push(features[index].name);
+                }
+                }
+
+
+                var sp = document.getElementById("spells");
+                sp.innerHTML="";
+                if(spells!==undefined){
+                    sp.innerHTML= "<br>You have the following spell slots (plus those added by your spellcasting modifier):";
+                    var spellLevels=[];
+                    var spellLists = [];
+                    var spellno = 0;
+                for (let index = 0; index < Object.values(spells).length; index++) {
+                    if(Object.values(spells)[index]!==0)
+                    {
+                        sp.innerHTML+= "<br>Level "+index + ": "+Object.values(spells)[index];
+                        character.spellSlots.push(Object.values(spells)[index]);
+                        spellLevels.push(index);
+                        spellno+=parseInt(Object.values(spells)[index]);
+                    }
+                }
+                
+                var sa = document.getElementById("spellAmount");
+                sa.innerHTML= "<br>You may choose up to "+spellno+" spells, in the proportion specified above <br><br>";
+
+                var selectSpells = document.getElementById("selectSpells");
+
+                for (const key in spellLevels) {
+                        spellLists = this.getSpellsByLevel(userClass,key);
+                        spellLists.then((a) => {
+                            var spellNames=[];
+                            for (const name in a) {
+                                spellNames.push(a[name].name);
+                                    
+                            }
+                            if(document.getElementById("level"+key)==null)
+                            {
+                                var div = document.createElement('div');
+                                div.id = 'level'+key;
+                                div.innerHTML= "Level "+key+"<br>";
+                            }
+                            else{
+                                div = document.getElementById("level"+key);
+                                div.innerHTML= "Level "+key+"<br>";
+                                
+                            }
+                                
+
+                            for (let index = 0; index < spellNames.length; index++) {
+                                var checkbox = document.createElement('input');
+                                checkbox.type = "checkbox";
+                                checkbox.value = 1;
+                                checkbox.name = spellNames[index];
+                                div.appendChild(checkbox);
+                                div.appendChild(document.createTextNode(spellNames[index]));
+                                div.innerHTML += "<br>"
+                            }
+                            div.innerHTML += "<br>"
+                            selectSpells.append(div);
+                        });
+                        
+                    }
+                
+                    
+
+            }
+                var cs = document.getElementById("classSpecs");
+                cs.innerHTML="";
+                cs.innerHTML="Any other class variables or restrictions (i.e Wild Shape's restrictions) are shown here:"
+                for (let index = 0; index < Object.values(class_specifics).length; index++) {
+                    if(Object.values(class_specifics)[index]!==0 || Object.values(class_specifics)[index]!=="[]" )
+                    {
+                        cs.innerHTML+="<br>"+Object.keys(class_specifics)[index]+": "+Object.values(class_specifics)[index]
+                    }
+                    
+                }
+
+            })
+            .catch(error => {
+                console.error(error);
+            });
+        }
+    }
+
+    render() {
+        return (
+            <FormControl className={useStyles.formControl}>
+                <InputLabel id="demo-simple-select-helper-label">Level</InputLabel>
+                <Select value={this.state.level} onChange={this.handleChange}>
+                    <MenuItem value=""><em>None</em></MenuItem>
+                    <MenuItem value="1">1</MenuItem>
+                    <MenuItem value="2">2</MenuItem>
+                    <MenuItem value="3">3</MenuItem>
+                    <MenuItem value="4">4</MenuItem>
+                    <MenuItem value="5">5</MenuItem>
+                    <MenuItem value="6">6</MenuItem>
+                    <MenuItem value="7">7</MenuItem>
+                    <MenuItem value="8">8</MenuItem>
+                    <MenuItem value="9">9</MenuItem>
+                    <MenuItem value="10">10</MenuItem>
+                    <MenuItem value="11">11</MenuItem>
+                    <MenuItem value="12">12</MenuItem>
+                    <MenuItem value="13">13</MenuItem>
+                    <MenuItem value="14">14</MenuItem>
+                    <MenuItem value="15">15</MenuItem>
+                    <MenuItem value="16">16</MenuItem>
+                    <MenuItem value="17">17</MenuItem>
+                    <MenuItem value="18">18</MenuItem>
+                    <MenuItem value="19">19</MenuItem>
+                    <MenuItem value="20">20</MenuItem>
+                </Select>
+                <FormHelperText>Select your level</FormHelperText>
+            </FormControl>
+        );
+    }
+};
 
 class CharacterCreate extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userClass: null
+        }
+
+        this.handleCallback = this.handleCallback.bind(this);
+    }
+
+    handleCallback = (childData) => {
+        this.setState({
+            userClass: childData
+        })
+    }
+
     render() {
+        const {userClass} = this.state;
+
         return (
             <div>
                 <title>Character Creation</title>
-                {/*<LevelSelection />*/}
-                <ClassSelection />
+
+                <ClassSelection parentCallback = {this.handleCallback}/> 
+                {userClass}
+
+                <LevelSelection parentToChild = {userClass}/>
             </div>
         );
     }
