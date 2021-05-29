edf6a9c473a25ab445a52b4ffbf098dc88a7eda0
Juan <juanfgort@gmail.com>
2021-05-29 13:11:02 -0500

    my job here is done

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ app.post('/characterEdit', function(req, res) {
 
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`DnD character creator running at http://localhost:${port}`)
   })
 
 module.exports = app;
